0d70e8f2ed71aca09e4a1e90193c55e846cc46f3
sepanta <163876726+Sepanta7@users.noreply.github.com>
2024-10-30 19:12:53 +0330

    change fonts

--- index.php
+++ index.php
@@ -78,10 +78,14 @@ $users = file('data/users.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <title>z-panel</title>
     <style>
         body {
             background-color: #2c3e50;
+            font-family: "Inter", sans-serif;
         }
         .userspanel {
             background-color: #34495e;
